72f7d340fefc1fbb594c55eabb628c6e853c6db8
Islam Yakout <117403825+c0depirate69@users.noreply.github.com>
2025-04-12 22:24:40 +0200

    Delete Web Sec/CPD directory

--- Web Sec/CPD/mitm/rewrite_equals.py
+++
@@ -1,6 +0,0 @@
-from mitmproxy import http
-import re
-
-def response(flow: http.HTTPFlow) -> None:
-    if 'text/javascript' in flow.response.headers.get('content-type', ''):
-        flow.response.text = re.sub(r'===', '==', flow.response.text)
